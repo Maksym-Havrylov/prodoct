70d2a76d94cc2e026d0850c8a4559cb61e0e447d
Maksym Havrylov <salomondr007@gmail.com>
2021-05-14 12:08:43 +0300

    first

---
+++ js/script.js
@@ -0,0 +1,355 @@
+"use strict";
+// function testWebP(callback) {
+//   let webP = new Image();
+//   webP.onload = webP.onerror = function () {
+//     callback(webP.height == 2);
+//   };
+//   webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+// }
+// testWebP(function (support) {
+//   if (support == true) {
+//     document.querySelector('body').classList.add('webp');
+//   }
+// })
+function testWebP(callback) {
+  var webP = new Image();
+  webP.onload = webP.onerror = function () {
+    callback(webP.height == 2);
+  };
+  webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+}
+
+testWebP(function (support) {
+  if (support == true) {
+    document.querySelector('body').classList.add('webp');
+  } else {
+    document.querySelector('body').classList.add('no-webp');
+  }
+});;
+;
+
+/* --- burger --- */
+$(document).ready(function () {
+  $('.icon-menu').click(function (event) {
+    $('.icon-menu,.menu__body').toggleClass('_active');
+    $('body').toggleClass('lock');
+  });
+}); /* --- burger end --- */
+
+/* --- modal --- */
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}; /* --- modal end --- */
+
+/* --- toggle (accordion) --- */
+const acc = document.getElementsByClassName("faq__accordion");
+let k;
+
+for (k = 0; k < acc.length; k++) {
+  acc[k].addEventListener("click", function () {
+    this.classList.toggle("active");
+    let panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}; /* --- toggle (accordion) end --- */
+
+/* --- slider__slick ---*/
+$(document).ready(function () {
+  $('.slider').slick({
+    infinite: true,
+    arrows: false,
+    focusOnSelect: true,
+    pauseOnDotsHover: true,
+    centerMode: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    // dots: true,
+    responsive: [{
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 3,
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+        }
+      }
+    ]
+  });
+}); /* --- slider__slick end ---*/
+
+/* --- custom-select in calculator ---*/
+var x, i, j, l, ll, selElmnt, a, b, c;
+/*look for any elements with the class "custom-select":*/
+x = document.getElementsByClassName("custom-select");
+l = x.length;
+for (i = 0; i < l; i++) {
+  selElmnt = x[i].getElementsByTagName("select")[0];
+  ll = selElmnt.length;
+  /*for each element, create a new DIV that will act as the selected item:*/
+  a = document.createElement("DIV");
+  a.setAttribute("class", "select-selected");
+  a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
+  x[i].appendChild(a);
+  /*for each element, create a new DIV that will contain the option list:*/
+  b = document.createElement("DIV");
+  b.setAttribute("class", "select-items select-hide");
+  for (j = 1; j < ll; j++) {
+    /*for each option in the original select element,
+    create a new DIV that will act as an option item:*/
+    c = document.createElement("DIV");
+    c.innerHTML = selElmnt.options[j].innerHTML;
+    c.addEventListener("click", function(e) {
+        /*when an item is clicked, update the original select box,
+        and the selected item:*/
+        var y, i, k, s, h, sl, yl;
+        s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+        sl = s.length;
+        h = this.parentNode.previousSibling;
+        for (i = 0; i < sl; i++) {
+          if (s.options[i].innerHTML == this.innerHTML) {
+            s.selectedIndex = i;
+            h.innerHTML = this.innerHTML;
+            y = this.parentNode.getElementsByClassName("same-as-selected");
+            yl = y.length;
+            for (k = 0; k < yl; k++) {
+              y[k].removeAttribute("class");
+            }
+            this.setAttribute("class", "same-as-selected");
+            break;
+          }
+        }
+        h.click();
+    });
+    b.appendChild(c);
+  }
+  x[i].appendChild(b);
+  a.addEventListener("click", function(e) {
+      /*when the select box is clicked, close any other select boxes,
+      and open/close the current select box:*/
+      e.stopPropagation();
+      closeAllSelect(this);
+      this.nextSibling.classList.toggle("select-hide");
+      this.classList.toggle("select-arrow-active");
+    });
+}
+function closeAllSelect(elmnt) {
+  /*a function that will close all select boxes in the document,
+  except the current select box:*/
+  var x, y, i, xl, yl, arrNo = [];
+  x = document.getElementsByClassName("select-items");
+  y = document.getElementsByClassName("select-selected");
+  xl = x.length;
+  yl = y.length;
+  for (i = 0; i < yl; i++) {
+    if (elmnt == y[i]) {
+      arrNo.push(i)
+    } else {
+      y[i].classList.remove("select-arrow-active");
+    }
+  }
+  for (i = 0; i < xl; i++) {
+    if (arrNo.indexOf(i)) {
+      x[i].classList.add("select-hide");
+    }
+  }
+}
+/*if the user clicks anywhere outside the select box,
+then close all select boxes:*/
+document.addEventListener("click", closeAllSelect);
+/* --- custom-select in calculator end ---*/
+
+/* --- quantity ---*/
+$('.minus-btn').on('click', function (e) {
+  e.preventDefault();
+  var $this = $(this);
+  var $input = $this.closest('div').find('input');
+  var value = parseInt($input.val());
+
+  if (value > 1) {
+    value = value - 1;
+  } else {
+    value = 0;
+  }
+
+  $input.val(value);
+
+});
+
+$('.plus-btn').on('click', function (e) {
+  e.preventDefault();
+  var $this = $(this);
+  var $input = $this.closest('div').find('input');
+  var value = parseInt($input.val());
+
+  if (value < 100) {
+    value = value + 1;
+  } else {
+    value = 100;
+  }
+
+  $input.val(value);
+}); /* --- quantity end ---*/
+
+/* --- calendar datepicker ---*/
+
+ /* --- calendar datepicker end ---*/
+
+ /* --- multisteps-form ---*/
+
+//DOM elements
+const DOMstrings = {
+  stepsBtnClass: 'multisteps-form__progress-btn',
+  stepsBtns: document.querySelectorAll(`.multisteps-form__progress-btn`),
+  stepsBar: document.querySelector('.multisteps-form__progress'),
+  stepsForm: document.querySelector('.multisteps-form__form'),
+  stepsFormTextareas: document.querySelectorAll('.multisteps-form__textarea'),
+  stepFormPanelClass: 'multisteps-form__panel',
+  stepFormPanels: document.querySelectorAll('.multisteps-form__panel'),
+  stepPrevBtnClass: 'js-btn-prev',
+  stepNextBtnClass: 'js-btn-next'
+}; //remove class from a set of items
+
+const removeClasses = (elemSet, className) => {
+  elemSet.forEach(elem => {
+    elem.classList.remove(className);
+  });
+}; //return exect parent node of the element
+
+
+const findParent = (elem, parentClass) => {
+  let currentNode = elem;
+
+  while (!currentNode.classList.contains(parentClass)) {
+    currentNode = currentNode.parentNode;
+  }
+
+  return currentNode;
+}; //get active button step number
+
+
+const getActiveStep = elem => {
+  return Array.from(DOMstrings.stepsBtns).indexOf(elem);
+}; //set all steps before clicked (and clicked too) to active
+
+
+const setActiveStep = activeStepNum => {
+  //remove active state from all the state
+  removeClasses(DOMstrings.stepsBtns, 'js-active'); //set picked items to active
+
+  DOMstrings.stepsBtns.forEach((elem, index) => {
+    if (index <= activeStepNum) {
+      elem.classList.add('js-active');
+    }
+  });
+}; //get active panel
+
+
+const getActivePanel = () => {
+  let activePanel;
+  DOMstrings.stepFormPanels.forEach(elem => {
+    if (elem.classList.contains('js-active')) {
+      activePanel = elem;
+    }
+  });
+  return activePanel;
+}; //open active panel (and close unactive panels)
+
+
+const setActivePanel = activePanelNum => {
+  //remove active class from all the panels
+  removeClasses(DOMstrings.stepFormPanels, 'js-active'); //show active panel
+
+  DOMstrings.stepFormPanels.forEach((elem, index) => {
+    if (index === activePanelNum) {
+      elem.classList.add('js-active');
+      setFormHeight(elem);
+    }
+  });
+}; //set form height equal to current panel height
+
+
+const formHeight = activePanel => {
+  const activePanelHeight = activePanel.offsetHeight;
+  DOMstrings.stepsForm.style.height = `${activePanelHeight}px`;
+};
+
+const setFormHeight = () => {
+  const activePanel = getActivePanel();
+  formHeight(activePanel);
+}; //STEPS BAR CLICK FUNCTION
+
+
+DOMstrings.stepsBar.addEventListener('click', e => {
+  //check if click target is a step button
+  const eventTarget = e.target;
+
+  if (!eventTarget.classList.contains(`${DOMstrings.stepsBtnClass}`)) {
+    return;
+  } //get active button step number
+
+
+  const activeStep = getActiveStep(eventTarget); //set all steps before clicked (and clicked too) to active
+
+  setActiveStep(activeStep); //open active panel
+
+  setActivePanel(activeStep);
+}); //PREV/NEXT BTNS CLICK
+
+DOMstrings.stepsForm.addEventListener('click', e => {
+  const eventTarget = e.target; //check if we clicked on `PREV` or NEXT` buttons
+
+  if (!(eventTarget.classList.contains(`${DOMstrings.stepPrevBtnClass}`) || eventTarget.classList.contains(`${DOMstrings.stepNextBtnClass}`))) {
+    return;
+  } //find active panel
+
+
+  const activePanel = findParent(eventTarget, `${DOMstrings.stepFormPanelClass}`);
+  let activePanelNum = Array.from(DOMstrings.stepFormPanels).indexOf(activePanel); //set active step and active panel onclick
+
+  if (eventTarget.classList.contains(`${DOMstrings.stepPrevBtnClass}`)) {
+    activePanelNum--;
+  } else {
+    activePanelNum++;
+  }
+
+  setActiveStep(activePanelNum);
+  setActivePanel(activePanelNum);
+}); //SETTING PROPER FORM HEIGHT ONLOAD
+
+window.addEventListener('load', setFormHeight, false); //SETTING PROPER FORM HEIGHT ONRESIZE
+
+window.addEventListener('resize', setFormHeight, false); //changing animation via animation select !!!YOU DON'T NEED THIS CODE (if you want to change animation type, just change form panels data-attr)
+
+const setAnimationType = newType => {
+  DOMstrings.stepFormPanels.forEach(elem => {
+    elem.dataset.animation = newType;
+  });
+}; //selector onchange - changing animation
+
+
+const animationSelect = document.querySelector('.pick-animation__select');
+animationSelect.addEventListener('change', () => {
+  const newAnimationType = animationSelect.value;
+  setAnimationType(newAnimationType);
+});
+ /* --- multisteps-form end ---*/
